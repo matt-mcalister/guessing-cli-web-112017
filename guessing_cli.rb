75117e17324ba1c1ae55c1c5cb6653adf7e7f41f
Matt McAlister <matt.mcalister93@gmail.com>
2017-11-09 15:58:18 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,6 +1,6 @@
 # Code your solution here!
 def run_guessing_game
-  puts "Guess a number between 1 and 6."
+  # puts "Guess a number between 1 and 6."
   response = gets.chomp
 
   while response != "exit"
@@ -19,4 +19,5 @@ def run_guessing_game
     #   response = gets.chomp
     end
   end
+  puts "Goodbye!"
 end
